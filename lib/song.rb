912b2310b5411159e4202563dd20a8731cc98273
rukia <ruqayashar1@gmail.com>
2023-05-30 23:10:30 +0300

    completed

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,45 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :name, :album, :id
 
-  def initialize(name:, album:)
+  def initialize(name:, album:, id: nil)
+    @id = id
     @name = name
     @album = album
   end
 
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.album)
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+    # return the Ruby instance
+    self
+  end
+  def self.create(name:, album:)
+    song = Song.new(name: name, album: album)
+    song.save
+  end
 end
+# song = Song.create(name: "Hello", album: "25")
+# # => #<Song:0x007f94f2c28ee8 @id=1, @name="Hello", @album="25">
+
+# song.name
+# # => "Hello"
+
+# song.album
